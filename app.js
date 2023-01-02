e26b80f4a515da6304e9d23dc887b4a3545aa771
farohan <68710283+farohan@users.noreply.github.com>
2023-01-02 13:30:42 -0800

    Update app.js

--- app.js
+++ app.js
@@ -83,3 +83,187 @@ const hslToHex = (hslColor) => {
     return `#${f(0)}${f(8)}${f(4)}`.toUpperCase();
 };
 
+const convertRGBtoHSL = (rgbValues) => {
+    return rgbValues.map((pixel) => {
+        let hue, 
+            saturation,
+            luminance = 0;
+
+        let redOpposite = pixel.r / 255;
+        let greenOpposite = pixel.g / 255;
+        let blueOpposite = pixel.b / 255;
+
+        const cMax = Math.max(redOpposite, greenOpposite, blueOpposite);
+        const cMin = Math.min(redOpposite, greenOpposite, blueOpposite);
+
+        const difference = cMax - cMin;
+        luminance = (cMax + cMin) / 2.0;
+
+        if (luminance <= 0.5) {
+            saturation = difference / (cMax + cMin);
+        } else if (luminance >= 0.5) {
+            saturation = difference / (2.0 - cMax - cMin);
+        }
+
+        const maxColorValue = Math.max(pixel.r, pixel.g, pixel.b);
+
+        if (maxColorValue === pixel.r) {
+            hue = (greenOpposite - blueOpposite) / difference;
+        } else if (maxColorValue === pixel.g) {
+            hue = 2.0 + (blueOpposite - redOpposite) / difference;
+        } else {
+            hue = 4.0 + (greenOpposite - blueOpposite) / difference;
+        }
+
+        hue *= 60;
+
+        if (hue < 0) {
+            hue += 360;
+        }
+
+        if (difference === 0) {
+            return false;
+        }
+
+        return {
+            h: Math.round(hue) + 180,
+            s: parseFloat(saturation * 100).toFixed(2),
+            l: parseFloat(luminance * 100).toFixed(2),
+        };
+    });
+};
+
+const orderByLuminance = (rgbValues) => {
+    const calculateLuminance = (p) => {
+        return 0.2126 * p.r + 0.7152 * p.g + 0.0722 * p.b;
+    };
+
+    return rgbValues.sort((p1, p2) => {
+        return calculateLuminance(p2) - calculateLuminance(p1);
+    });
+};
+
+const buildRgb = (imageData) => {
+    const rgbValues = [];
+
+    for (let i = 0; i < imageData.length; i += 4) {
+        const rgb = {
+            r: imageData[i],
+            g: imageData[i + 1],
+            b: imageData[i + 2]
+        };
+
+        rgbValues.push(rgb);
+    }
+
+    return rgbValues;
+};
+
+const calculateColorDifference = (color1, color2) => {
+    const rDifference = Math.pow(color2.r - color1.r, 2);
+    const gDifference = Math.pow(color2.g - color1.g, 2);
+    const bDifference = Math.pow(color2.b - color1.b, 2);
+
+    return rDifference + gDifference + bDifference;
+};
+
+const findBiggestColorRange = (rgbValues) => {
+    let rMin = Number.MAX_VALUE;
+    let gMin = Number.MAX_VALUE;
+    let bMin = Number.MAX_VALUE;
+
+    let rMax = Number.MIN_VALUE;
+    let gMax = Number.MIN_VALUE;
+    let bMax = Number.MIN_VALUE;
+
+    rgbValues.forEach((pixel) => {
+        rMin = Math.min(rMin, pixel.r);
+        gMin = Math.min(gMin, pixel.g);
+        bMin = Math.min(bMin, pixel.b);
+    
+        rMax = Math.max(rMax, pixel.r);
+        gMax = Math.max(gMax, pixel.g);
+        bMax = Math.max(bMax, pixel.b);
+    });
+
+    const rRange = rMax - rMin;
+    const gRange = gMax - gMin;
+    const bRange = bMax - bMin;
+
+    const biggestRange = Math.max(rRange, gRange, bRange);
+    if (biggestRange === rRange) {
+        return 'r';
+    } else if (biggestRange === gRange) {
+        return 'g';
+    } else {
+        return 'b';
+    }
+};
+
+const quantization = (rgbValues, depth) => {
+    const MAX_DEPTH = 4;
+
+    if (depth === MAX_DEPTH || rgbValues.length === 0) {
+        const color = rgbValues.reduce(
+            (prev, curr) => {
+                prev.r += curr.r;
+                prev.g += curr.g;
+                prev.b += curr.b;
+
+                return prev;
+            },
+            {
+                r: 0,
+                g: 0,
+                b: 0
+            }
+        );
+
+        color.r = Math.round(color.r / rgbValues.length);
+        color.g = Math.round(color.g / rgbValues.length);
+        color.b = Math.round(color.b / rgbValues.length);
+
+        return [color];
+    }
+
+    const componentToSortBy = findBiggestColorRange(rgbValues);
+    rgbValues.sort((p1, p2) => {
+        return p1[componentToSortBy] - p2[componentToSortBy];
+    });
+
+    const mid = rgbValues.length / 2;
+    return [
+        ...quantization(rgbValues.slice(0, mid), depth + 1),
+        ...quantization(rgbValues.slice(mid + 1), depth + 1),
+    ];
+};
+
+const main = () => {
+    const imgFile = document.querySelector('#img-file');
+    const image = new Image();
+    const file = imgFile.files[0];
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+        image.onload = () => {
+            const canvas = document.querySelector('#canvas');
+            canvas.width = image.width;
+            canvas.height = image.height;
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(image, 0, 0);
+
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            const rgbArray = buildRgb(imageData.data);
+            const quantColors = quantization(rgbArray, 0);
+            buildPalette(quantColors);
+        };
+        image.src = fileReader.result;
+    };
+    fileReader.readAsDataURL(file);
+};
+
+main();
+
+
+
+
